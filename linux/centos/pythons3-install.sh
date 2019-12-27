96645fa27bc8ea963833a203a83551eab4e19cbe
ricky yang <ricky.yang@ef.com>
2019-12-27 21:07:38 +0800

    add python install

---
+++ linux/centos/pythons3-install.sh
@@ -0,0 +1,9 @@
+sudo yum -y install zlib*
+sudo wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz&&tar zxvf Python-3.8.0.tgz&&mkdir /usr/local/python3&&cd Python-3.8.0&&./configure --prefix=/usr/local/python3 &&make && make install
+ln -s /usr/local/python3/bin/python3 /usr/bin/python3
+ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
+## Update env
+PATH=/usr/local/python3/bin:$PATH:$HOME/bin
+export PATH
+## make sure it takes effect
+source ~/.bash_profile
