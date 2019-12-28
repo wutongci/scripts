74fddd1bffb2fb72cc716b0c8a293b349a2f2aef
ricky yang <ricky.yang@ef.com>
2019-12-28 23:50:26 +0800

    Fix issue: unable to send and receive message

--- kafka/install.sh
+++ kafka/install.sh
@@ -3,4 +3,12 @@ cd /usr/local/src && tar -xzf kafka_2.11-2.2.0.tgz -C /opt/
 cd /opt && mv kafka_2.11-2.2.0 kafka && mkdir kafka/log
 # update server.properties
 cd /opt/kafka/bin/
+# start kafka
 # ./kafka-server-start.sh -daemon ../config/server.properties
+
+# send message
+./kafka-console-producer.sh --broker-list 10.128.42.59:9092 --topic demo1
+# receive message
+./kafka-console-consumer.sh --bootstrap-server 10.128.42.118:9092 --from-beginning --topic demo1
+# receive message
+./kafka-console-consumer.sh --bootstrap-server 10.128.42.78:9092 --from-beginning --topic demo1
