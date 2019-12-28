75c4ca532d00c8aec48e0f2248930d9f6275f190
ricky yang <ricky.yang@ef.com>
2019-12-28 20:41:48 +0800

    update kafka install

--- kafka/install.sh
+++ kafka/install.sh
@@ -1,3 +1,6 @@
 wget http://mirrors.tuna.tsinghua.edu.cn/apache/kafka/2.2.0/kafka_2.11-2.2.0.tgz -P /usr/local/src
 cd /usr/local/src && tar -xzf kafka_2.11-2.2.0.tgz -C /opt/
 cd /opt && mv kafka_2.11-2.2.0 kafka && mkdir kafka/log
+# update server.properties
+cd /opt/kafka/bin/
+# ./kafka-server-start.sh -daemon ../config/server.properties
