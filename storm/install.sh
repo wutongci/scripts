a26a9b34f68d9aec7f7e4a7b3ff71a15d92d43cb
ricky.yang <ricky.yang@ef.com>
2019-12-29 22:15:24 +0800

    Fix typo

--- storm/install.sh
+++ storm/install.sh
@@ -1,3 +1,3 @@
 wget http://mirror.bit.edu.cn/apache/storm/apache-storm-2.0.0/apache-storm-2.0.0.tar.gz -P /usr/local/src/
 cd /usr/local/src && tar zxvf apache-storm-2.0.0.tar.gz -C /opt
-cd /opt && mv apache-storm-2.0.0.tar.gz storm
+cd /opt && mv apache-storm-2.0.0 storm
