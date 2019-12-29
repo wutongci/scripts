e956b2c5139a8c16ed53f66eb37a1ee668bd389f
ricky.yang <ricky.yang@ef.com>
2019-12-29 23:19:56 +0800

    update start-torm-all.sh

--- storm/start-storm-all.sh
+++ storm/start-storm-all.sh
@@ -6,31 +6,19 @@ nimbusServers='10.128.42.59 10.128.42.78'
 supervisorServers='10.128.42.59 10.128.42.78 10.128.42.118'
 
 #启动所有的nimbus
-for nim in $nimbusServers
-do
-    ssh -T $nim <<EOF
-        nohup /opt/storm/bin/storm nimbus >/dev/null 2>&1 &
-EOF
+nohup /opt/storm/bin/storm nimbus >/dev/null 2>&1 &
 echo 从节点 $nim 启动nimbus...[ done ]
 sleep 1
 done
 
 #启动所有的ui
-for u in $nimbusServers
-do
-    ssh -T $u <<EOF
-        nohup /opt/storm/bin/storm ui >/dev/null 2>&1 &
-EOF
+nohup /opt/storm/bin/storm ui >/dev/null 2>&1 &
 echo 从节点 $u 启动ui...[ done ]
 sleep 1
 done
 
 #启动所有的supervisor
-for visor in $supervisorServers
-do
-    ssh -T $visor <<EOF
-        nohup /opt/storm/bin/storm supervisor >/dev/null 2>&1 &
-EOF
+nohup /opt/storm/bin/storm supervisor >/dev/null 2>&1 &
 echo 从节点 $visor 启动supervisor...[ done ]
 sleep 1
 done
