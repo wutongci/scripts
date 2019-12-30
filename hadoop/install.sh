a0f9a2bd4aa2d7dc49b319cd43cd70f7ff3a5940
ricky yang <ricky.yang@ef.com>
2019-12-30 17:32:51 +0800

    add hadoop

---
+++ hadoop/install.sh
@@ -0,0 +1,3 @@
+wget -P http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz -P /usr/local/src
+cd /usr/loca/src && tar -xzf hadoop-3.2.0.tar.gz -C /opt
+cd /opt && mv hadoop-3.2.0 hadoop
