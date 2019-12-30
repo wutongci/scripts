9853c5cf2e1cd23b3a4e5b9b3f9ed488a2e2bdf7
ricky yang <ricky.yang@ef.com>
2019-12-30 17:49:19 +0800

    Fix typo

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -1,3 +1,3 @@
-wget -P http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz -P /usr/local/src
+wget http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz -P /usr/local/src
 cd /usr/local/src && tar -xzf hadoop-3.2.1.tar.gz -C /opt
 cd /opt && mv hadoop-3.2.1 hadoop
