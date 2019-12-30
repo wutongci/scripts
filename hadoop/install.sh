0f20a2215ef6f2bf88e507af8eb2efbbb8169fef
ricky yang <ricky.yang@ef.com>
2019-12-30 17:46:23 +0800

    update hadoop install.sh

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -1,3 +1,3 @@
-wget -P http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz -P /usr/local/src
-cd /usr/loca/src && tar -xzf hadoop-3.2.0.tar.gz -C /opt
-cd /opt && mv hadoop-3.2.0 hadoop
+wget -P http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz -P /usr/local/src
+cd /usr/local/src && tar -xzf hadoop-3.2.1.tar.gz -C /opt
+cd /opt && mv hadoop-3.2.1 hadoop
