5305afc8e61f036340b4746cd7e3eb01d00b3e4b
ricky yang <ricky.yang@ef.com>
2019-12-31 17:43:41 +0800

    add sctivation-1.1.jar

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -3,6 +3,6 @@ cd /usr/local/src && tar -xzf hadoop-3.2.1.tar.gz -C /opt
 cd /opt && mv hadoop-3.2.1 hadoop
 # config host file, ie, vim /etc/hosts
 # config env variable, vim /etc/profile
-# copy xxx-site.xml file to /opt/hadoop/etc/hadoop
+# copy xxx-site.xml file, workers, hadoop-env.sh  to /opt/hadoop/etc/hadoop
 # grant other machines ssh permission
 # start hadoop cluster ./sbin/start-all.sh
