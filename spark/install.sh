ced29df2947fd0d213e1973b0d22f75c8c0e1df4
ricky.yang <ricky.yang@ef.com>
2020-01-01 00:20:02 +0800

    update

--- spark/install.sh
+++ spark/install.sh
@@ -1,3 +1,3 @@
-wget https://www.apache.org/dyn/closer.lua/spark/spark-3.0.0-preview2/spark-3.0.0-preview2-bin-hadoop3.2.tgz -P  /usr/local/src
+wget http://mirror.bit.edu.cn/apache/spark/spark-3.0.0-preview2/spark-3.0.0-preview2-bin-hadoop3.2.tgz -P  /usr/local/src
 cd /usr/local/src && tar -xzf spark-3.0.0-preview2-bin-hadoop3.2.tgz -C /opt/
 cd /opt && mv spark-3.0.0-preview2-bin-hadoop3.2 spark
