7bc925ad4efbe4f9f9c370227f93070ae500679f
ricky yang <ricky.yang@ef.com>
2019-12-31 10:15:46 +0800

    add hadoop image

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -1,3 +1,8 @@
 wget http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz -P /usr/local/src
 cd /usr/local/src && tar -xzf hadoop-3.2.1.tar.gz -C /opt
 cd /opt && mv hadoop-3.2.1 hadoop
+# config host file, ie, vim /etc/hosts
+# config env variable, vim /etc/profile
+# copy xxx-site.xml file to /opt/hadoop/etc/hadoop
+# grant other machines ssh permission
+# start hadoop cluster ./sbin/start-all.sh
