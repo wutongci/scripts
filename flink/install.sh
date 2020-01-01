6d00d9351134c8bc0091d17ba6f850c746cbc74b
ricky.yang <ricky.yang@ef.com>
2020-01-01 18:05:45 +0800

    add flink install.sh

---
+++ flink/install.sh
@@ -0,0 +1,3 @@
+wget https://mirror.bit.edu.cn/apache/flink/flink-1.9.1/flink-1.9.1-bin-scala_2.12.tgz -P /usr/local/src
+cd /usr/local/src && tar -xzf flink-1.9.1-bin-scala_2.12.tgz -C /opt/
+cd /opt && mv flink-1.9.1 flink
