465d072b23b8ccd4c80cbece76626f340b987f55
ricky.yang <ricky.yang@ef.com>
2020-01-01 16:15:04 +0800

    add hbase README.md

---
+++ hbase/README.md
@@ -0,0 +1,11 @@
+* How to install hbase?
+  * run install.sh
+  * update /etc/profile
+  ```
+  export HBASE_HOME=/opt/hbase
+  export PATH=$PATH:$HBASE_HOME/bin
+  ```
+* How to start hbase?
+  ```
+    ./bin/start-hbase.sh
+  ```
--- hbase/install.sh
+++ hbase/install.sh
@@ -1,3 +1,3 @@
 wget https://mirror.bit.edu.cn/apache/hbase/2.2.2/hbase-2.2.2-bin.tar.gz -P  /usr/local/src
 cd /usr/local/src && tar -xzf hbase-2.2.2-bin.tar.gz -C /opt/
-cd /opt && mv hbase-2.2.2-bin hbase
+cd /opt && mv hbase-2.2.2 hbase
