890b87f48d9e3f2a081c6ecb85fd29c4e0f14d5d
ricky yang <ricky.yang@ef.com>
2020-01-19 17:08:14 +0800

    update typo

--- elasticsearch/README.md
+++ elasticsearch/README.md
@@ -6,5 +6,6 @@
   * max virtual memory areas vm.max_map_count [65530] is too low, increase to at least [262144]
     ```
     echo "vm.max_map_count=262144" > /etc/sysctl.conf
-    sysctl -P
+    sysctl -p
     ```
+  *  exception caught on transport layer?
--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -1,3 +1,3 @@
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.4.2.tar.gz -P /usr/loca/src
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.4.2.tar.gz -P /usr/local/src
 cd /usr/local/src && tar -xzf elasticsearch-6.4.2.tar.gz -C /opt/
 cd /opt/ && mv elasticsearch-6.4.2 elasticsearch
