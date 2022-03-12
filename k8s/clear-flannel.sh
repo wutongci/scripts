e02bc4255d52797450786e633254c64f602ecc89
Ricky Yang <ricky.yang@ef.com>
2022-03-12 11:28:45 +0800

    update flannel:remove unnecessary stuff

--- k8s/clear-flannel.sh
+++ k8s/clear-flannel.sh
@@ -1,8 +1,4 @@
-sudo ifconfig cni0 down
-sudo ip link delete cni0
 sudo ifconfig flannel.1 down
 sudo ip link delete flannel.1
-sudo rm -rf /var/lib/cni/
-sudo rm -f /etc/cni/net.d/*
 sudo /etc/init.d/networking restart
 sudo service network restart
